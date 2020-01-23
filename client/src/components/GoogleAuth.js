de2dab60c4a88e45c2109aa1d41d7ed50f12a6ba
chiragsrvstv <chirag.srivastava@live.com>
2020-01-23 09:29:03 +0530

    authentication functionality authenticatoin with oaut using component states

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,19 +1,50 @@
 import React from "react";
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null }; // we don't know of the user is signed in or nut, that's why null
   componentDidMount() {
     window.gapi.load("client: auth2", () => {
-      window.gapi.client.init({
-        clientId: '628635127890-jnooauh03n2uvqonmq6ed9ocfinum58d.apps.googleusercontent.com',
-        scope: 'email'
-      }).then(() => {
-        this.auth = window.gapi.auth2.getAuthInstance();
-      })
+      window.gapi.client
+        .init({
+          clientId:
+            "628635127890-jnooauh03n2uvqonmq6ed9ocfinum58d.apps.googleusercontent.com",
+          scope: "email"
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
     });
   }
 
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      // return <div> I dont know if we are signed in </div>;
+      return null;
+    } else if (this.state.isSignedIn) {
+      return (
+        <button className="ui red google button">
+          {" "}
+          <i className="google icon" />
+          Sign Out
+        </button>
+      );
+    } else {
+      return (
+        <button className="ui red google button">
+          <i className="google icon" />
+          Sign In With Google
+        </button>
+      );
+    }
+  }
+
   render() {
-    return <div> Google Auth </div>;
+    return <div> {this.renderAuthButton()} </div>;
   }
 }
 
