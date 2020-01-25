01b3e0591d342d1b7bc9235db19548363fcb5b4b
chiragsrvstv <chirag.srivastava@live.com>
2020-01-25 23:31:32 +0530

    redux form squash
    redux-form
    
    redux-form

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,6 +3,10 @@
 import {combineReducers} from 'redux';
 import authReducer from './authReducer';
 
+// importing pre-provided reducer from redux-form
+import {reducer} from 'redux-form';
+
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  form: reducer
 });
