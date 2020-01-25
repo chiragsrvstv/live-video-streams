775365bd13aa282de8af3f1ed3a6da59009797e0
chiragsrvstv <chirag.srivastava@live.com>
2020-01-25 09:22:14 +0530

    action type refactor

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,7 @@
 // action creators
+
+import {SIGN_IN, SIGN_OUT} from './types'
+
 export const signIn = () => {
   return {
     type: "SIGN_IN"
---
+++ client/src/actions/types.js
@@ -0,0 +1,3 @@
+// avoiding typos by creating a seperate file for all the strings used in combineReducers
+export const SIGN_IN = 'SIGN_IN';
+export const SIGN_OUT = 'SIGN_OUT';
