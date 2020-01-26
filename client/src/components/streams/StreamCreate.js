e25a44fdd71b9f4ee7c035c99f897ab67de6464b
chiragsrvstv <chirag.srivastava@live.com>
2020-01-26 17:00:47 +0530

    form handling

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -2,23 +2,37 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
+  // the below method diplays form on screen with the help of Field component
   renderInput(formProps) {
-    console.log(formProps);
+    // console.log(formProps);
     return (
       <div className="field">
-        <label for="name"> {formProps.label} </label>
-        <input {...formProps.input} />
+        <label> {formProps.label} </label>
+        <input required {...formProps.input} />
       </div>
     );
-
-    // above all the properties of formProps are being applied to input element
+    // above, all the properties of formProps are being applied to input element
   }
+
+  // method for form handling
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
   render() {
     // console.log(this.props);
     return (
-      <form className="ui form">
-        <Field name="title" component={this.renderInput} label="Enter Title"/>
-        <Field name="description" component={this.renderInput} label="Enter Description"/>
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form"
+      >
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
+        <button className="ui primary button"> Submit </button>
       </form>
     );
   }
