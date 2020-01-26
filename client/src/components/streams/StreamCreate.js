4f73369487faaf7b2b4da1fd92f1e163e2c6feb3
chiragsrvstv <chirag.srivastava@live.com>
2020-01-26 16:15:29 +0530

    form values fetching values from form and saving them to redux store

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -2,12 +2,17 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
+  renderInput(formProps) {
+    console.log(formProps);
+    return <input {...formProps.input} />;
+     // above all the properties of formProps are being applied to input element
+  }
   render() {
     // console.log(this.props);
     return (
       <form>
-        <Field name="title" />
-        <Field name="description" />
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
       </form>
     );
   }
