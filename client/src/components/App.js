594cea0feebad6bba33fdea528a31bd8db600c96
chiragsrvstv <chirag.srivastava@live.com>
2020-01-26 16:26:43 +0530

    form styles

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -10,7 +10,7 @@ import Header from './Header.js'
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
          <div>
            <Header />
--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -4,15 +4,21 @@ import { Field, reduxForm } from "redux-form";
 class StreamCreate extends React.Component {
   renderInput(formProps) {
     console.log(formProps);
-    return <input {...formProps.input} />;
-     // above all the properties of formProps are being applied to input element
+    return (
+      <div className="field">
+        <label for="name"> {formProps.label} </label>
+        <input {...formProps.input} />
+      </div>
+    );
+
+    // above all the properties of formProps are being applied to input element
   }
   render() {
     // console.log(this.props);
     return (
-      <form>
-        <Field name="title" component={this.renderInput} />
-        <Field name="description" component={this.renderInput} />
+      <form className="ui form">
+        <Field name="title" component={this.renderInput} label="Enter Title"/>
+        <Field name="description" component={this.renderInput} label="Enter Description"/>
       </form>
     );
   }
