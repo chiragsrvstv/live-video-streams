936385cd1358f18fd80269c0d5284db1228c7562
chiragsrvstv <chirag.srivastava@live.com>
2020-01-30 23:41:28 +0530

    actions dispatch

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,7 @@
 // action creators
 
 import streams from '../apis/streams';
-import {SIGN_IN, SIGN_OUT} from './types'
+import {SIGN_IN, SIGN_OUT, CREATE_STREAM} from './types'
 
 export const signIn = (userId) => {
   return {
@@ -21,6 +21,8 @@ export const createStream = (formValues) => {
   // using redux thunk
   return async (dispatch) => {
     // posting the form values at /streams
-    streams.post('/streams', formValues);
+    const response = await streams.post('/streams', formValues);
+    // dispatching actionCreator with payload as data fetched by axios
+    dispatch({type: CREATE_STREAM, payload: response.data});
   };
 };
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,3 +1,4 @@
 // avoiding typos by creating a seperate file for all the strings used in combineReducers
 export const SIGN_IN = 'SIGN_IN';
 export const SIGN_OUT = 'SIGN_OUT';
+export const CREATE_STREAM = 'CREATE_STREAM';
