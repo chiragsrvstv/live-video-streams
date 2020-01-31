69366b2439a5468b535beb64cee966bdfdfaf8c9
chiragsrvstv <chirag.srivastava@live.com>
2020-01-31 23:57:16 +0530

    more actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,9 +1,17 @@
 // action creators
 
-import streams from '../apis/streams';
-import {SIGN_IN, SIGN_OUT, CREATE_STREAM} from './types'
+import streams from "../apis/streams";
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM
+} from "./types";
 
-export const signIn = (userId) => {
+export const signIn = userId => {
   return {
     type: "SIGN_IN",
     payload: userId
@@ -17,12 +25,22 @@ export const signOut = () => {
 
 //
 
-export const createStream = (formValues) => {
+export const createStream = formValues => {
   // using redux thunk
-  return async (dispatch) => {
+  return async dispatch => {
     // posting the form values at /streams
-    const response = await streams.post('/streams', formValues);
+    const response = await streams.post("/streams", formValues);
     // dispatching actionCreator with payload as data fetched by axios
-    dispatch({type: CREATE_STREAM, payload: response.data});
+    dispatch({ type: CREATE_STREAM, payload: response.data });
   };
+};
+
+export const fetchStreams = () => async dispatch => {
+  const response = await streams.get("/streams");
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+export const fetchStream = () => async dispatch => {
+  const response = await streams.get("/streams/${id}");
+  dispatch({ type: FETCH_STREAM, payload: response.data });
 };
