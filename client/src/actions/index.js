c68c1a1d39d352784f0e5206f0cada6de0bdf426
chiragsrvstv <chirag.srivastava@live.com>
2020-02-01 01:02:05 +0530

    actions defining all the action creators

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -40,7 +40,17 @@ export const fetchStreams = () => async dispatch => {
   dispatch({ type: FETCH_STREAMS, payload: response.data });
 };
 
-export const fetchStream = () => async dispatch => {
+export const fetchStream = id => async dispatch => {
   const response = await streams.get("/streams/${id}");
   dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+  const response = await streams.put("/streams/${id}", formValues);
+  dispatch({ type: EDIT_STREAM, payload: response.data });
+};
+
+export const deleteStream = id => async dispatch => {
+  await streams.delete("/streams/${id}");
+  dispatch({ type: DELETE_STREAM, payload: id });
 };
