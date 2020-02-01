f8b4865326f73be52a631e39f41379fd70be3395
chiragsrvstv <chirag.srivastava@live.com>
2020-02-01 11:18:36 +0530

    fetching streams wiring up fetchStreams to connect, eventually to redux

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {fetchStreams} from '../../actions';
 
-const StreamList = () => {
-  return <div>StreamList</div>;
-};
+// const StreamList = () => {
+//   return <div>StreamList</div>;
+// };
 
-export default StreamList;
+class StreamList extends React.Component {
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
+  render() {
+    return <div> StreamList </div>
+  }
+}
+
+export default connect(null, {fetchStreams})(StreamList);
