c16593a99b946686f57bfef3212f8b0881227db6
chiragsrvstv <chirag.srivastava@live.com>
2020-02-01 11:03:52 +0530

    fetch streams reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,11 +2,13 @@
 
 import {combineReducers} from 'redux';
 import authReducer from './authReducer';
+import streamReducer from './streamReducer.js'
 
 // importing pre-provided reducer from redux-form
 import {reducer} from 'redux-form';
 
 export default combineReducers({
   auth: authReducer,
-  form: reducer
+  form: reducer,
+  streams: streamReducer
 });
--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -5,14 +5,24 @@ import {
   CREATE_STREAM,
   EDIT_STREAM,
   DELETE_STREAM
-} from "../action/types";
+} from "../actions/types";
+
+
 
 export default (state = {}, action) => {
   switch (action.type){
 
+    case FETCH_STREAMS:
+      // a new obj is being created by taking current records. mapKeys take all
+      // of the data from payload and convert that into a new object with keys
+      // equal to the property specified after the comma(id here), ... at
+      // mapKeys mean that a new obj is to be created everytime that is
+      // holding the prev. values as well.
+      return {...state, ..._.mapKeys(action.payload, 'id')};
+
     case FETCH_STREAM:
     // below is an es6 syntax for key interpolation, its a key value not an array.
-      return {..state, [action.payload.id]: action.payload};
+      return {...state, [action.payload.id]: action.payload};
 
     case CREATE_STREAM:
       return {...state, [action.payload.id]: action.payload};
@@ -21,7 +31,7 @@ export default (state = {}, action) => {
       return {...state, [action.payload.id]: action.payload};
 
     case DELETE_STREAM:
-    // here the payload is id itself, check action creator
+    // here the payload is id itself, check action creator. Using lodash here.
       return _.omit(state, action.payload);
 
 
