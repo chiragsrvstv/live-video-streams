2ea0a3624e7f71c2d09d85c611cfd6696f255a84
chiragsrvstv <chirag.srivastava@live.com>
2020-02-01 10:37:08 +0530

    delete reducer lodash

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import {
   FETCH_STREAM,
   FETCH_STREAMS,
@@ -8,13 +9,22 @@ import {
 
 export default (state = {}, action) => {
   switch (action.type){
+
     case FETCH_STREAM:
     // below is an es6 syntax for key interpolation, its a key value not an array.
       return {..state, [action.payload.id]: action.payload};
+
     case CREATE_STREAM:
       return {...state, [action.payload.id]: action.payload};
+
     case EDIT_STREAM:
       return {...state, [action.payload.id]: action.payload};
+
+    case DELETE_STREAM:
+    // here the payload is id itself, check action creator
+      return _.omit(state, action.payload);
+
+
     default:
     return state;
   }
