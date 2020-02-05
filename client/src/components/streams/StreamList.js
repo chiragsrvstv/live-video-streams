a7e7e856bf14ddf1865dd35728e74f05ab7aca88
chiragsrvstv <chirag.srivastava@live.com>
2020-02-05 17:11:42 +0530

    edit/delete

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -11,10 +11,23 @@ class StreamList extends React.Component {
     this.props.fetchStreams();
   }
 
+  // this helper method compares current userID with that associated to stream to authenticate users
+  renderAdmin(stream) {
+    if ((stream.userId === this.props.currentUserId)) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary"> Edit </button>
+            <button className="ui button negative"> Delete </button>
+        </div>
+      );
+    }
+  }
+
   renderList() {
     return this.props.streams.map(stream => {
       return (
         <div className="item" key={stream.id}>
+          {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera " />
           <div className="content">
             {stream.title}
@@ -36,7 +49,10 @@ class StreamList extends React.Component {
 
 const mapStateToProps = state => {
   // we have streams in the form of objs, so, turning objs to array
-  return { streams: Object.values(state.streams) };
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId
+  };
 };
 
 export default connect(
