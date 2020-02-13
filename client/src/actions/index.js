2401a0fb95fd772c7a524837178069f3da709674
chiragsrvstv <chirag.srivastava@live.com>
2020-02-13 23:32:54 +0530

    action creator correction

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -46,16 +46,16 @@ export const fetchStreams = () => async dispatch => {
 };
 
 export const fetchStream = id => async dispatch => {
-  const response = await streams.get("/streams/${id}");
+  const response = await streams.get(`/streams/${id}`);
   dispatch({ type: FETCH_STREAM, payload: response.data });
 };
 
 export const editStream = (id, formValues) => async dispatch => {
-  const response = await streams.put("/streams/${id}", formValues);
+  const response = await streams.put(`/streams/${id}`, formValues);
   dispatch({ type: EDIT_STREAM, payload: response.data });
 };
 
 export const deleteStream = id => async dispatch => {
-  await streams.delete("/streams/${id}");
+  await streams.delete(`/streams/${id}`);
   dispatch({ type: DELETE_STREAM, payload: id });
 };
