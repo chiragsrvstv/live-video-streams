ddf339e14a59254aa67cc244c9fa512f7c493ebb
chiragsrvstv <chirag.srivastava@live.com>
2020-02-15 17:28:04 +0530

    modal reusability

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import history from "../history";
 
 const Modal = props => {
   // return values
@@ -13,7 +12,7 @@ const Modal = props => {
     // with an event handler, to prevent this we can add an event handler to
     // child as well. (Whenever event occurs we get event object e)
     <div
-      onClick={() => history.push("/")}
+      onClick={props.onDissmiss}
       className="ui dimmer modals visible active"
     >
       // onClick event handler to exit modal only when user clicks in background
@@ -22,14 +21,9 @@ const Modal = props => {
         onClick={e => e.stopPropagation()}
         className="ui standard modal visible active"
       >
-        <div className="header"> Delete Stream </div>
-        <div className="content">
-          Are you sure you want to delete this Stream ?
-        </div>
-        <div className="actions">
-          <button className="ui primary button"> Delete </button>
-          <button className="ui button"> Cancel </button>
-        </div>
+        <div className="header"> {props.title} </div>
+        <div className="content">{props.content}</div>
+        <div className="actions">{props.actions} </div>
       </div>
     </div>,
     document.querySelector("#modal")
--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,10 +1,29 @@
 import React from "react";
 import Modal from "../Modal";
+import history from '../../history';
 
 const StreamDelete = () => {
+  // assigning two things(buttons here) from a single actions variable isn't a
+  // valid JS, so we decided to wrap them with a div but that div
+  // didn't worked along with semanticUi, So, using React fragment
+  // which will maintain our styling.
+  // React fragment is a jsx looking element that doesn't produce any
+  // HTML, almost invisible
+  const actions = (
+    <React.Fragment>
+      <button className="ui button negative"> Delete </button>
+      <button className="ui button"> Cancel </button>
+    </React.Fragment>
+  );
   return (
     <div className="ui container">
-      <Modal />
+      StreamDelete
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this Stream ?"
+        actions={actions}
+        onDissmiss={()=> history.push('/')}
+      />
     </div>
   );
 };
