5cdf18016c181005efe1800d8b5bd509b3a339ac
chiragsrvstv <chirag.srivastava@live.com>
2020-02-16 19:57:56 +0530

    building player

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -10,7 +10,33 @@ class StreamShow extends React.Component {
     this.videoRef = React.createRef();
   }
   componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+    const id = this.props.match.params.id
+    console.log(this.videoRef);
+
+    this.props.fetchStream(id);
+
+    this.buildPlayer();
+  }
+
+  // we handled loading the video player when the component would render
+  // initially, above. Here we want the player to be rendered any time the
+  // StreamShow component gets re-rendered or gets updated
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  buildPlayer() {
+    if(this.player || !this.props.stream) {
+      return;
+    }
+    // setting up player,
+    const id = this.props.match.params.id
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load(); // not trying to automatically play the video, only user can.
   }
 
   render() {
