e4e9e371c3dead321c393b27ec487763e36cdd66
chiragsrvstv <chirag.srivastava@live.com>
2020-02-17 23:47:01 +0530

    readme rename

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Live Video Streams
+
+This web application is made with React and Redux.
---
+++ client/build/precache-manifest.69014d0d846e34c34b5b3cb3c005ddcf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d73f5f73af67c613ae5d4105967febfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fca50ad14ec8427cfafb",
+    "url": "/static/js/2.ba53f742.chunk.js"
+  },
+  {
+    "revision": "fc5e3a145372d6627a07a7ad4ee03596",
+    "url": "/static/js/2.ba53f742.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e9cd6172ac85c4bb0e6a",
+    "url": "/static/js/main.e32b85fb.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
